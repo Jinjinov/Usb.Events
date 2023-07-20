674de6ebd0442769f5fd538e57cba7be8e0838c0
Urban <dzindzinovic@hotmail.com>
2023-07-20 14:27:24 +0200

    Version 11.0.0.1

--- Usb.Events/entrypoint.sh
+++ Usb.Events/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#gcc -march=armv7-a -o UsbEventWatcher.Linux_arm.so -shared -fPIC UsbEventWatcher.Linux.c -ludev
+#gcc -march=armv7-a -o UsbEventWatcher.Linux.so -shared -fPIC UsbEventWatcher.Linux.c -ludev
 
-#gcc -march=armv8-a -o UsbEventWatcher.Linux_arm64.so -shared -fPIC UsbEventWatcher.Linux.c -ludev
+#gcc -march=armv8-a -o UsbEventWatcher.Linux.so -shared -fPIC UsbEventWatcher.Linux.c -ludev
 
 #exec "$@"
