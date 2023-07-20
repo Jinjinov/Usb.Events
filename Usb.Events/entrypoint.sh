500537ab9de02589111882329e6b183a65a12e7a
Urban <dzindzinovic@hotmail.com>
2023-07-20 14:40:09 +0200

    Version 11.0.0.1

--- Usb.Events/entrypoint.sh
+++ Usb.Events/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "Started compiling"
+
 #gcc -march=armv7-a -o UsbEventWatcher.Linux.so -shared -fPIC UsbEventWatcher.Linux.c -ludev
 
 #gcc -march=armv8-a -o UsbEventWatcher.Linux.so -shared -fPIC UsbEventWatcher.Linux.c -ludev
+
+echo "Finished compiling"
 
 #exec "$@"
