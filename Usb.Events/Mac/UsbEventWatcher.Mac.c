718c081751c1227dfb896b27a6b8a0b78c54e300
hexx.one <5312542+hexxone@users.noreply.github.com>
2025-11-23 23:12:53 +0100

    formatting

--- Usb.Events/Mac/UsbEventWatcher.Mac.c
+++ Usb.Events/Mac/UsbEventWatcher.Mac.c
@@ -246,7 +246,7 @@ void get_usb_device_info(io_service_t device, int newdev)
 
     debug_print("USB device %s: %s\n", newdev ? "FOUND" : "REMOVED", devicename);
 
-    // Safe copy for DeviceName
+    // Safe copy for DeviceName and other fields
     strncpy(usbDevice.DeviceName, devicename, sizeof(usbDevice.DeviceName) - 1);
     usbDevice.DeviceName[sizeof(usbDevice.DeviceName) - 1] = '\0';
 
@@ -502,8 +502,7 @@ void configure_and_start_notifier(void)
     }
 
     io_iterator_t deviceRemovedIter;
-    addResult = IOServiceAddMatchingNotification(notificationPort, kIOTerminatedNotification, matchDict,
-                                                 usb_device_removed, NULL, &deviceRemovedIter);
+    addResult = IOServiceAddMatchingNotification(notificationPort, kIOTerminatedNotification, matchDict,usb_device_removed, NULL, &deviceRemovedIter);
 
     if (addResult != KERN_SUCCESS)
     {
